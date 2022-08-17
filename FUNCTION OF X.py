650dbe442f122fce2366ecd683d591988964d7bb
Umeshhiregoudar <umeshhiregoudar17@gmail.com>
2022-08-17 15:17:03 +0530

    Recursion is used inside the factorial function

--- FUNCTION OF X.py
+++ FUNCTION OF X.py
@@ -17,24 +17,9 @@ class function_of_x:
 
 
     def facto(self,num):
-
-       factorial = 1
-
-       if num < 0:
-
-          print(" Factorial does not exist for negative numbers")
-
-       elif num == 0:
-
-          print("The factorial of 0 is 1")
-
-       else:
-
-          for i in range(1,num + 1):
-
-             factorial = factorial*i
-
-       return factorial
+       if num == 0:
+           return 1
+       return num * self.facto(num-1)
 
 
 
