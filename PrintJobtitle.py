95b3e22844447eb802da813f25892853d4318316
Umeshhiregoudar <umeshhiregoudar17@gmail.com>
2022-09-28 09:04:50 +0530

    Getting the Jobtitles from the webpage and printing the same

--- PrintJobtitle.py
+++ PrintJobtitle.py
@@ -4,20 +4,16 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome("C:\\Users\\uhiregou\\PycharmProjects\\FirstProject\\Drivers\\chromedriver_win32\\chromedriver.exe")
 driver.get("https://jobs.hp.com/software-engineer-one/?job_type[]=Experienced&business_unit[]=china&sort=title&dir=ascending")
 driver.maximize_window()
-elements = driver.find_elements(By.CLASS_NAME,"jobTitle")
-for i in elements:
-    print(i.text)
-while(True):
-    try:
-        driver.find_element(By.XPATH,"//a[@class='button inactive btn-next']").click()
-    except:
-        break
-    time.sleep(10)
-    elements = driver.find_elements(By.CLASS_NAME,"jobTitle")
-    for i in elements:
-        print(i.text)
+element = driver.find_element(By.XPATH,"//div[contains(text(),'Showing Jobs')]")
+length=int(((element.text)[23:]))
+temp=0
+for i in range(length):
+     elements = driver.find_elements(By.CLASS_NAME, "jobTitle")
+     temp = temp + len(elements)
+     for i in elements:
+         print(i.text)
+     if(length==temp):
+         break
+     driver.find_element(By.XPATH, "//a[@class='button inactive btn-next']").click()
+     time.sleep(10)
 driver.close()
-
-
-
-
